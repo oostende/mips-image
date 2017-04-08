dc5c2e7ebd3901614ce09db29f4a409e9c6a9e33
oostende <redouaneelrhachi@hotmail.com>
2017-04-08 11:06:51 +0200

    Update skin.py

--- skin.py
+++ skin.py
@@ -79,7 +79,7 @@ def skin_user_skinname():
 
 # example: loadSkin("nemesis_greenline/skin.xml")
 config.skin = ConfigSubsection()
-DEFAULT_SKIN = "GigabluePax/skin.xml"
+DEFAULT_SKIN = "GigabluePax-FHD/skin.xml"
 # DEFAULT_SKIN = SystemInfo["HasFullHDSkinSupport"] and "PLi-FullNightHD/skin.xml" or "PLi-HD/skin.xml"
 # on SD hardware, PLi-HD will not be available
 if not fileExists(resolveFilename(SCOPE_SKIN, DEFAULT_SKIN)):
